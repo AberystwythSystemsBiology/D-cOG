cbac39ef3efacd64f78233902da2ec65c4a52753
KeironO <keiron.oshea@gmail.com>
2020-03-26 17:05:40 +0000

    [I] Added AA

--- dcog/main.py
+++ dcog/main.py
@@ -1,5 +1,10 @@
 import os
 from .data import DataPrepper
 
-# Load the data and export it to a file suited for DL.
+# Load the data and export it to a means suited for DL.
+eggnog_proteins_fp = ""
+nog_annotations_fp = ""
+
+
 DataPrepper()
+
